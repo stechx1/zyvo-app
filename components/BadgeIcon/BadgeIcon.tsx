80f66e354e3af38014dcd55339d1896b46dd0b4d
Summar_Raja <summar.raja@globewyze.com>
2023-11-11 11:29:14 +0500

    Update BadgeIcon.tsx

--- components/BadgeIcon/BadgeIcon.tsx
+++ components/BadgeIcon/BadgeIcon.tsx
@@ -16,7 +16,7 @@ export const BadgeIcon: React.FC<BadgeIconProps> = ({
       <div className="relative">
         <Image src={src} alt={alt} width={width} height={height} />
         {showBadge && (
-          <div className="absolute top-0 right-0 bg-primary-emerald-300 text-black rounded-full px-2 py-1 text-xs font-semibold grid place-items-center translate-x-[0.5rem] translate-y-[-0.75rem]">
+          <div className="absolute top-0 right-0 bg-primary-emerald-300 text-black rounded-full px-2 py-1 text-xs font-semibold grid place-items-center translate-x-[0.5rem] translate-y-[-0.75rem] bg-secondary-green">
             {badgeCount}
           </div>
         )}
