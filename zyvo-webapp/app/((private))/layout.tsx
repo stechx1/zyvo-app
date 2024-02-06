e5fbc2b7b5545f2a5695343483bb6e498be222ec
Summar_Raja <summar.raja@globewyze.com>
2024-02-06 19:09:47 +0500

    fixed background

--- zyvo-webapp/app/((private))/layout.tsx
+++ zyvo-webapp/app/((private))/layout.tsx
@@ -4,8 +4,8 @@ export default function PrivateLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='px-5 sm:px-[6%] xl:px-30 xl:py-8 lg:py-8 md:py-8 sm:py-8 py-2 font-Poppins'
-    // sm:bg-[url("/images/dotted-background.png")] bg-cover bg-center
+    <div className='px-5 sm:px-[6%] xl:px-30 xl:py-8 lg:py-8 md:py-8 sm:py-8 py-2 font-Poppins sm:bg-[url("/images/dotted-background.png")] bg-no-repeat'
+    
     >
       {children}
     </div>
